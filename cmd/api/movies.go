f9aa8c1d4621c10c5a2b7a7283670cd95b239d6c
jrvldam <jrvldam@gmail.com>
2023-07-15 02:18:11 +0200

    enveloping responses

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -28,7 +28,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		Version:   1,
 	}
 
-	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
 		app.logger.Print(err)
 		http.Error(w, "The server encountered a problem and clould not process your request", http.StatusInternalServerError)
