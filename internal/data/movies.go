af76218bde2710dd8a698873ba1eeb3206c38fcc
jrvldam <jrvldam@gmail.com>
2023-07-18 22:54:51 +0200

    creating a new movie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com.jrvldam.greenlight/internal/validator"
+	"github.com/lib/pq"
 )
 
 type Movie struct {
@@ -39,7 +40,15 @@ type MovieModel struct {
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil
+	query := `
+    INSERT INTO movies (title, year, runtime, genres)
+    VALUES ($1, $2, $3, $4)
+    RETURNING id, created_at, version`
+
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	// Using query to get returning values
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
