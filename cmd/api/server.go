6934bad00239e8a827037d7df6f633ecf21f8305
jrvldam <jrvldam@gmail.com>
2023-08-08 01:20:18 +0200

    intercepting shutdown signals

---
+++ cmd/api/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", app.config.port),
+		Handler: app.routes(),
+		// ErrorLog:     log.New(logger, "", 0),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+
+		app.logger.PrintInfo("caught signal", map[string]string{
+			"signal": s.String(),
+		})
+
+		os.Exit(0)
+	}()
+
+	app.logger.PrintInfo("starting service", map[string]string{
+		"addr": srv.Addr,
+		"env":  app.config.env,
+	})
+
+	return srv.ListenAndServe()
+}
