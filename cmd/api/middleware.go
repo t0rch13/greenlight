5508c1554b532f98f099231d4abda390c7f1c5f7
jrvldam <jrvldam@gmail.com>
2023-08-14 02:00:15 +0200

    preflight cors requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -185,12 +185,21 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Vary", "Origin")
 
+		w.Header().Set("Vary", "Access-Control-Request-Method")
+
 		origin := r.Header.Get("Origin")
 
 		if origin != "" {
 			for _, trustedOrigin := range app.config.cors.trustedOrigins {
 				if trustedOrigin == origin {
 					w.Header().Set("Access-Control-Allow-Origin", origin)
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+						w.WriteHeader(http.StatusOK)
+						return
+					}
 					break
 				}
 			}
